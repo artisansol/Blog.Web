84737732b55782cecca732696d3744452fe5ae64
artisansol <patrickremos@artisanitsolutions.com>
2023-06-20 22:53:06 +0400

    Add project files.

---
+++ Blog.Web/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blog.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            AddRootDirectory(builder.Services);
+            builder.Services.AddLogging();
+            builder.Services.AddHttpClient();
+
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+            app.Run();
+        }
+
+        private static void AddRootDirectory(IServiceCollection services)
+        {
+            services.AddRazorPages(options =>
+            {
+                options.RootDirectory = "/Views/Pages";
+            });
+        }
+    }
+}
