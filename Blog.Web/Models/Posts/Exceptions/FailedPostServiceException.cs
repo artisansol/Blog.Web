a9bed3990fdb6e39c922490267f8fff50ce1f445
artisansol <patrickremos@artisanitsolutions.com>
2023-07-02 15:44:10 +0400

    ShouldThrowServiceExceptionOnRemoveIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ Blog.Web/Models/Posts/Exceptions/FailedPostServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace Blog.Web.Models.Posts.Exceptions
+{
+    public class FailedPostServiceException : Xeption
+    {
+        public FailedPostServiceException(Exception innerException) 
+            : base(message: "Failed post service error occurred, contact support.",
+                innerException)
+        { }
+    }
+}
---
+++ Blog.Web/Models/Posts/Exceptions/PostServiceException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace Blog.Web.Models.Posts.Exceptions
+{
+    public class PostServiceException : Xeption
+    {
+        public PostServiceException(Xeption innerException) : 
+            base(message: "Post service error occurred, contact support.", 
+                innerException)
+        { }
+    }
+}
