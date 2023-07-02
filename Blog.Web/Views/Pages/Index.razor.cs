cb0fe519e0f3519cf4794bbae64d3c7fbf2e45a8
artisansol <patrickremos@artisanitsolutions.com>
2023-07-02 20:12:57 +0400

    BASES: Dialog base

---
+++ Blog.Web/Views/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Blog.Web.Views.Bases;
+using Microsoft.AspNetCore.Components;
+
+namespace Blog.Web.Views.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public DialogBase Dialog { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            this.Dialog.Show();
+        }
+
+        public void CloseDialog()
+        {
+            Dialog.Hide();
+        }
+    }
+}
