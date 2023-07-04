28b8b2d0fcb5949862ef44bfc64619e5ce4120ea
artisansol <patrickremos@artisanitsolutions.com>
2023-07-04 20:17:34 +0400

    BASES: Card Base

---
+++ Blog.Web/Views/Bases/CardBase.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blog.Web.Views.Bases
+{
+    public partial class CardBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string SubTitle { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+    }
+}
