bae2da224a5f30abfca4f7a4413aced8434b3cfb
artisansol <patrickremos@artisanitsolutions.com>
2023-07-04 18:30:14 +0400

    ShouldThrowServiceExceptionIfServiceErrorOccursAndLogItAsync -> FAIL

--- Blog.Web.Unit.Tests/Services/Views/PostViews/PostViewServiceTests.Exceptions.RetrieveAll.cs
+++ Blog.Web.Unit.Tests/Services/Views/PostViews/PostViewServiceTests.Exceptions.RetrieveAll.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blog.Web.Models.Posts.Exceptions;
 using Blog.Web.Models.PostViews;
 using Blog.Web.Models.PostViews.Exceptions;
 using Moq;
@@ -41,6 +42,43 @@ namespace Blog.Web.Unit.Tests.Services.Views.PostViews
             this.loggingBrokerMock.Verify(broker => 
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedPostViewDependencyException))),
+                    Times.Once());
+
+            this.postServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedPostViewServiceException = 
+                new FailedPostViewServiceException(serviceException);
+
+            var expectedPostViewServiceException = 
+                new PostViewServiceException(failedPostViewServiceException);
+
+            this.postServiceMock.Setup(service => 
+                service.RetrieveAllPostsAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<List<PostView>> retrieveAllPostViewsTask = 
+                this.postViewService.RetrieveAllPostViewsAsync();
+
+            // then
+            await Assert.ThrowsAsync<PostViewServiceException>(() => 
+                retrieveAllPostViewsTask.AsTask());
+
+            this.postServiceMock.Verify(service => 
+                service.RetrieveAllPostsAsync(), 
+                Times.Once());
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostViewServiceException))), 
                     Times.Once());
 
             this.postServiceMock.VerifyNoOtherCalls();
---
+++ Blog.Web/Models/PostViews/Exceptions/FailedPostViewServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace Blog.Web.Models.PostViews.Exceptions
+{
+    public class FailedPostViewServiceException : Xeption
+    {
+        public FailedPostViewServiceException(Exception innerException)
+            : base(message: "Failed post view service error occurred, contact support.", innerException)
+        { }
+    }
+}
