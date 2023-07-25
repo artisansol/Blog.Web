eeee845bd8ed88c2c4d8152c65a0a89889b376bc
artisansol <patrickremos@artisanitsolutions.com>
2023-07-25 19:09:53 +0400

    ShouldThrowCriticalDependencyExeptionOnAddIfCriticalErrorOccursAndLogItAsync -> FAIL

---
+++ Blog.Web.Unit.Tests/Services/Foundations/Posts/PostServiceTests.Exceptions.Add.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Web.Models.Posts;
+using Blog.Web.Models.Posts.Exceptions;
+using Moq;
+using Xunit;
+
+namespace Blog.Web.Unit.Tests.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyExceptions))]
+        public async Task ShouldThrowCriticalDependencyExeptionOnAddIfCriticalErrorOccursAndLogItAsync(Exception criticalDependencyException)
+        {
+            // given
+            var somePost = CreateRandomPost();
+
+            var failedPostDependencyException = 
+                new FailedPostDependencyException(criticalDependencyException);
+
+            var expectedPostDependencyException = 
+                new PostDependencyException(failedPostDependencyException);
+
+            this.apiBrokerMock.Setup(broker => 
+                broker.PostPostAsync(somePost))
+                    .ThrowsAsync(failedPostDependencyException);
+
+            // when
+            ValueTask<Post> addPostTask = 
+                this.postService.AddPostAsync(somePost);
+
+            // then
+            await Assert.ThrowsAsync<PostDependencyException>(() => 
+                addPostTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker => 
+                broker.PostPostAsync(It.IsAny<Post>()), 
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPostDependencyException))), 
+                    Times.Once());
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
