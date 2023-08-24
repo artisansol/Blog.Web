08fa6eff3900d8eef67ee6d5a895bc06300c2677
Pratik Kaje <kajepratik@gmail.com>
2023-08-24 19:12:37 +0400

    WIP : Index Page Design

---
+++ Blog.Web/Views/Bases/AppBarBase.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Navigations;
+
+namespace Blog.Web.Views.Bases
+{
+    public partial class AppBarBase : ComponentBase
+    {
+        [Parameter]
+        public AppBarColor ColorMode { get; set; }
+
+        [Parameter]
+        public AppBarMode Mode { get; set; }
+
+        [Parameter]
+        public AppBarPosition Position { get; set; }
+
+        [Parameter]
+        public bool IsSticky { get; set; }
+
+        [Parameter]
+        public string CssClass { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+    }
+}
